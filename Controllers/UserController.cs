1ee98875c0272d60c51651997d518f6261588d5e
github_cloudlabsuser_2003@cloudlabsaiuser.com <github_cloudlabsuser_2003@cloudlabsaiuser.com>
2024-06-18 08:07:00 +0000

    Adding ARM templates to deploy the web app

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,18 +11,28 @@ namespace CRUD_application_2.Controllers
         public ActionResult Index()
         {
             // Implement the Index method here
+            return View(userlist);
+
         }
  
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            return View();
+
         }
  
         // POST: User/Create
@@ -30,6 +40,8 @@ namespace CRUD_application_2.Controllers
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            userlist.Add(user);
+            return RedirectToAction("Index");
         }
  
         // GET: User/Edit/5
@@ -37,6 +49,12 @@ namespace CRUD_application_2.Controllers
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // POST: User/Edit/5
@@ -48,12 +66,35 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+     
+            var userInList = userlist.FirstOrDefault(u => u.Id == id);
+            if (userInList == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Assuming User class has properties like Name, Email, etc.
+            // Update properties individually. This is more efficient and safer.
+            userInList.Name = user.Name;
+            userInList.Email = user.Email;
+            // Repeat for other properties as necessary
+
+            // No need to remove and re-add the user to the list
+            // as we're updating the existing object directly.
+
+            return RedirectToAction("Index");
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // POST: User/Delete/5
@@ -61,6 +102,12 @@ namespace CRUD_application_2.Controllers
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                userlist.Remove(user);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
